84d1dd994c34449f347313249c1f7170c2ab1f97
Robert Lemmens <rj.lemmens@pm.me>
2021-08-08 21:24:41 +0200

    bump version

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t registry.gitlab.com/notes26/notes-api/registration-server .
-docker push registry.gitlab.com/notes26/notes-api/registration-server
+docker build -t registry.gitlab.com/notes26/notes-api/registration-server:0.2.0 .
+docker push registry.gitlab.com/notes26/notes-api/registration-server:0.2.0
