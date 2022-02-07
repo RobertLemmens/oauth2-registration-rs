bd361cf07960542429749ffe980e8cfa33d5256b
Robert J. Lemmens <eksit@pm.me>
2022-02-07 02:50:49 +0000

    updated build scripts

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+if [[ $1 == 'dev' ]]
+then
+echo "Building dev image"
+docker build -t registry.gitlab.com/notes26/notes-api/registration-server:dev .
+docker push registry.gitlab.com/notes26/notes-api/registration-server:dev
+else
+echo "Building image"
 docker build -t registry.gitlab.com/notes26/notes-api/registration-server:0.2.0 .
 docker push registry.gitlab.com/notes26/notes-api/registration-server:0.2.0
+fi
