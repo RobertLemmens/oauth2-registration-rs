b96103efbcd5c656958d4aab13a9534a65831342
Robert Lemmens <rj.lemmens@pm.me>
2022-03-05 22:34:01 +0100

    working on homepage design

--- frontend/src/home.rs
+++ frontend/src/home.rs
@@ -35,21 +35,25 @@ impl Home {
         let history: AnyHistory = link.history().unwrap();
         let register_callback = Callback::from(move |_| history.push(Route::Register));
         html! {
-            <div class="columns is-centered">
-                <div class="column has-text-centered">
-                    <figure class="image has-image-centered">
-                      <img style="width:400px; margin-left:auto; margin-right:auto;" src="assets/notes_logo.png"/>
-                    </figure>
-                    <div class="title">
-                        <img />
-                        <h1>{"Markdown note taking without slowing down"}</h1>
-                    </div>
-                    <div class="subtitle">
-                        <p>{"Your one-stop shop for markdown note taking. Fast, clean, and available on all major platforms with device cloud sync."}</p>
-                    </div>
-                    <div class="action-buttons">
-                        <button onclick={register_callback} class="button is-primary">{"Start your free trial"}</button>
-                        <button class="button">{"Documentation"}</button>
+            <div class="banner-wrapper">
+                <div class="container">
+                    <div class="columns is-centered">
+                        <div class="column has-text-centered">
+                            <figure class="image has-image-centered">
+                              <img style="width:400px; margin-left:auto; margin-right:auto;" src="assets/notes_logo.png"/>
+                            </figure>
+                            <div class="title">
+                                <img />
+                                <h1>{"Markdown note taking without slowing down"}</h1>
+                            </div>
+                            <div class="subtitle">
+                                <p>{"Your one-stop shop for markdown note taking. Fast, clean, and available on all major platforms with device cloud sync."}</p>
+                            </div>
+                            <div class="action-buttons">
+                                <button onclick={register_callback} class="button is-primary">{"Start your free trial"}</button>
+                                <button class="button">{"Documentation"}</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -58,17 +62,41 @@ impl Home {
 
     fn features(&self, ctx: &Context<Self>) -> Html {
         html! {
-                <div class="columns is-centered">
-                    <div class="column">
-                    {self.card(ctx, "assets/markdown_text3.png", "Markdown", "Concise and familiar syntax. Take notes with ease", "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.")}
+            <div class="feature-wrapper">
+                <div class="container">
+                    <div class="columns is-centered">
+                        <div class="column">
+                        {self.card(ctx, "assets/markdown_text3.png", "Markdown", "Concise and familiar syntax. Take notes with ease", "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.")}
+                        </div>
+                        <div class="column">
+                        {self.card(ctx, "", "Cross platform", "Mobile, desktop, it doesn't matter", "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.")}
+                        </div>
+                        <div class="column">
+                        {self.card(ctx, "", "Cloud Sync", "Start at your desk, end on the couch. Everything is synced.", "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.")}
+                        </div>
                     </div>
-                    <div class="column">
-                    {self.card(ctx, "", "Cross platform", "Mobile, desktop, it doesn't matter", "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.")}
-                    </div>
-                    <div class="column">
-                    {self.card(ctx, "", "Cloud Sync", "Start at your desk, end on the couch. Everything is synced.", "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.")}
+
+                    <div class="columns is-centered">
+                        <div class="column">
+                        {self.card(ctx, "assets/markdown_text3.png", "Markdown", "Concise and familiar syntax. Take notes with ease", "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.")}
+                        </div>
+                        <div class="column">
+                        {self.card(ctx, "", "Cross platform", "Mobile, desktop, it doesn't matter", "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.")}
+                        </div>
+                        <div class="column">
+                        {self.card(ctx, "", "Cloud Sync", "Start at your desk, end on the couch. Everything is synced.", "Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.")}
+                        </div>
                     </div>
                 </div>
+            </div>
+        }
+    }
+
+    fn showcase(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div class="showcase-wrapper">
+                <p>{"Showcase"}</p>
+            </div>
         }
     }
 
@@ -90,16 +118,9 @@ impl Component for Home {
         let link = ctx.link();
         html! {
             <>
-            <div class="banner-wrapper">
-                <div class="container">
-                    {self.banner(ctx)}
-                </div>
-            </div>
-            <div class="feature-wrapper">
-                <div class="container">
-                    {self.features(ctx)}
-                </div>
-            </div>
+                {self.banner(ctx)}
+                {self.features(ctx)}
+                {self.showcase(ctx)}
             </>
         }
     }
